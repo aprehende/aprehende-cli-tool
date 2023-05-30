032c48febf2574e606fc9386c7f7f0d6bd02c6ee
Jordan Jaramillo <vednadroj@gmail.com>
2023-05-29 22:06:27 -0500

    feat: option to js components

--- src/utils.ts
+++ src/utils.ts
@@ -1,9 +1,9 @@
 import { join } from "path";
-import { green } from "colors";
-import { writeFileSync, readFileSync, mkdirpSync } from "fs-extra";
+import { blue, green } from "colors";
 import { compile } from "handlebars";
+import { writeFileSync, readFileSync, mkdirpSync } from "fs-extra";
 
-const templatesDir = `${__dirname}/templates`;
+const templatesComponentDir = `${__dirname}/templates/component`;
 
 interface IOptions {
   [key: string]: string | boolean | undefined;
@@ -18,41 +18,49 @@ export const createComponent = (componentName: string, options: IOptions) => {
   mkdirpSync(componentPath);
   console.log(green("Folder created successfully"));
 
-  const componentTemplate = readFileSync(
-    `${templatesDir}/component.hbs`,
-    "utf-8"
-  );
+  const isOnlyJs = options["onlyJs"] ? true : false;
+  const extension = isOnlyJs ? "js" : "ts";
+  let componentTemplatePath;
+
+  if (isOnlyJs)
+    componentTemplatePath = `${templatesComponentDir}/javascript/component.hbs`;
+  else
+    componentTemplatePath = `${templatesComponentDir}/typescript/component.hbs`;
+
+  const componentTemplate = readFileSync(componentTemplatePath, "utf-8");
 
   const componentTemplateContent = compile(componentTemplate)({
     componentName: formatedComponentName,
-    withCss: options["withCss"] ? true : false
+    withCss: options["withCss"] ? true : false,
+    withStyled: options["withStyled"] ? true : false,
   });
 
   writeFileSync(
-    `${componentPath}/${formatedComponentName}.tsx`,
+    `${componentPath}/${formatedComponentName}.${extension}x`,
     componentTemplateContent
   );
+  console.log(green("Component created successfully"));
 
-if(options?.withCss){
-  const cssTemplate = readFileSync(
-    `${templatesDir}/css.hbs`,
-    "utf-8"
-  );
+  if (options?.withCss) {
+    const cssTemplate = readFileSync(
+      `${templatesComponentDir}/css.hbs`,
+      "utf-8"
+    );
 
-  const cssTemplateContent = compile(cssTemplate)({
-    componentName: formatedComponentName,
-  });
+    const cssTemplateContent = compile(cssTemplate)({
+      componentName: formatedComponentName,
+    });
 
-  writeFileSync(
-    `${componentPath}/${formatedComponentName}.css`,
-    cssTemplateContent
-  );
-  console.log(green("Css created successfully"));
-}
+    writeFileSync(
+      `${componentPath}/${formatedComponentName}.css`,
+      cssTemplateContent
+    );
+    console.log(green("Css created successfully"));
+  }
 
   // TODO create barrels
   const indexComponentTemplate = readFileSync(
-    `${templatesDir}/barrel.hbs`,
+    `${templatesComponentDir}/barrel.hbs`,
     "utf-8"
   );
 
@@ -60,25 +68,31 @@ if(options?.withCss){
     componentName: formatedComponentName,
   });
 
-  writeFileSync(`${componentPath}/index.ts`, indexComponentTemplateContent);
+  writeFileSync(
+    `${componentPath}/index.${extension}`,
+    indexComponentTemplateContent
+  );
   console.log(green("Barrel created successfully"));
 
   if (options["withStyled"]) {
-    const styledTemplate = readFileSync(
-      `${templatesDir}/styled-component.hbs`,
-      "utf-8"
-    );
+    let styledTemplatePath;
+
+    if (isOnlyJs)
+      styledTemplatePath = `${templatesComponentDir}/javascript/styled-component.hbs`;
+    else
+      styledTemplatePath = `${templatesComponentDir}/typescript/styled-component.hbs`;
+
+    const styledTemplate = readFileSync(styledTemplatePath, "utf-8");
+
     const styledTemplateContent = compile(styledTemplate)({
       componentName: formatedComponentName,
     });
     writeFileSync(
-      `${componentPath}/${formatedComponentName}.styles.ts`,
+      `${componentPath}/${formatedComponentName}.styles.${extension}`,
       styledTemplateContent
     );
     console.log(green("Styled created successfully"));
   }
 
-  console.log(
-    green(`Creation of ${formatedComponentName} component completed`)
-  );
+  console.log(blue(`Creation of ${formatedComponentName} component completed`));
 };
