dc70e9a20ecfac860f64292d869c1be3a9997288
TitoWin94 <88687143+TitoWin94@users.noreply.github.com>
2023-05-30 20:16:57 -0500

    fix conflicts

--- src/actions/component.ts
+++ src/actions/component.ts
@@ -1,16 +1,16 @@
-import { join } from "path";
-import { blue } from "colors";
-import loading from "loading-cli";
-import { PATH } from "../constants";
-import { delay } from "../utilities";
-import { compile } from "handlebars";
-import { writeFileSync, readFileSync, mkdirpSync } from "fs-extra";
+import { join } from 'path';
+import { blue } from 'colors';
+import loading from 'loading-cli';
+import { PATH } from '../constants';
+import { delay } from '../utilities';
+import { compile } from 'handlebars';
+import { writeFileSync, readFileSync, mkdirpSync } from 'fs-extra';
 import {
   createStyles,
   createStyledComponent,
   createComponent as createComponentFunc,
   createSubcomponent,
-} from "./generators";
+} from '../generators';
 
 interface IOptions {
   [key: string]: string | boolean | undefined;
@@ -20,7 +20,7 @@ export const createComponent = async (
   componentName: string,
   options: IOptions
 ) => {
-  const loader = loading("Creating component").start();
+  const loader = loading('Creating component').start();
   const formatedComponentName =
     componentName.charAt(0).toUpperCase() + componentName.slice(1);
 
@@ -29,25 +29,25 @@ export const createComponent = async (
   await delay(500);
   mkdirpSync(componentPath);
 
-  const isOnlyJs = options["onlyJs"] ? true : false;
-  const extension = isOnlyJs ? "js" : "ts";
+  const isOnlyJs = options['onlyJs'] ? true : false;
+  const extension = isOnlyJs ? 'js' : 'ts';
   loader.clear();
-  loader.text = "Creating component";
+  loader.text = 'Creating component';
 
   await createComponentFunc({
     isOnlyJs,
     componentPath,
     componentName: formatedComponentName,
-    withCss: Boolean(options["withCss"]),
-    withStyled: Boolean(options["withStyled"]),
-    withComponents: Boolean(options["withComponents"]),
+    withCss: Boolean(options['withCss']),
+    withStyled: Boolean(options['withStyled']),
+    withComponents: Boolean(options['withComponents']),
   });
 
   loader.clear();
-  loader.text = "Creating barrel";
+  loader.text = 'Creating barrel';
   const indexComponentTemplate = readFileSync(
     `${PATH.COMPONENT_TEMPLATE}/barrel.hbs`,
-    "utf-8"
+    'utf-8'
   );
 
   const indexComponentTemplateContent = compile(indexComponentTemplate)({
@@ -60,27 +60,27 @@ export const createComponent = async (
     indexComponentTemplateContent
   );
 
-  if (options["withCss"]) {
+  if (options['withCss']) {
     loader.clear();
-    loader.text = "Creating css file";
+    loader.text = 'Creating css file';
     await createStyles({
       componentPath,
       componentName: formatedComponentName,
     });
   }
 
-  if (options["withStyled"]) {
+  if (options['withStyled']) {
     loader.clear();
-    loader.text = "Creating styled component";
+    loader.text = 'Creating styled component';
     await createStyledComponent({
       isOnlyJs,
       componentPath,
       componentName: formatedComponentName,
     });
   }
-  if (options["withComponents"]) {
+  if (options['withComponents']) {
     loader.clear();
-    loader.text = "Creating  subcomponents folder";
+    loader.text = 'Creating  subcomponents folder';
     await createSubcomponent({
       isOnlyJs,
       componentPath,
