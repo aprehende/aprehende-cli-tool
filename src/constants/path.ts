bd30058279e6a49d9f3f2350880863af69b4d90e
Jordan Jaramillo <vednadroj@gmail.com>
2023-06-01 18:41:35 -0500

    feat: generate empty file for template in /.aprehende/templates/

--- src/constants/path.ts
+++ src/constants/path.ts
@@ -1,6 +1,7 @@
 import path from 'path';
 
 export const PATH = {
+  CUSTOM_TEMPLATE: path.join(process.cwd(), '/.aprehende'),
   CONFIG_TEMPLATE: path.join(__dirname, '../templates/'),
   HOOK_TEMPLATE: path.join(__dirname, '../templates/hook'),
   COMPONENT_TEMPLATE: path.join(__dirname, '../templates/component'),
--- src/generators/index.ts
+++ src/generators/index.ts
@@ -2,6 +2,7 @@ export * from './init';
 export * from './hook';
 export * from './styles';
 export * from './subHook';
+export * from './template';
 export * from './component';
 export * from './subcomponent';
 export * from './styled-component';
